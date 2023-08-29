0c9c77c4394aac7d91ede101d6bb6106a94a11a9
Dharmesh_Saini <dharmesh_saini@epam.com>
2023-08-29 12:13:12 +0530

    Add POM and testNg test

---
+++ src/test/java/TestJenkins.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestJenkins {
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("Before method output");
+    }
+
+    @Test
+    public void test() {
+        System.out.println("test method output");
+    }
+}
